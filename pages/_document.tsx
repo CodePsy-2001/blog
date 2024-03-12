04bfac0e0aa5c7156391cfb7d154901ee584d611
CodePsy-2001 <codepsy2001@gmail.com>
2024-03-12 19:03:36 +0900

    소개페이지 얼개 완료

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   const meta = {
     title: "코드싸이 블로그",
-    description: "프로그래밍, 심리학, 문화/예술 이야기",
     image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
   };
 
@@ -11,16 +10,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="robots" content="follow, index" />
-        <meta name="description" content={meta.description} />
         <meta property="og:site_name" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
-        {/*<meta name="twitter:card" content="summary_large_image" />*/}
-        {/*<meta name="twitter:site" content="@yourname" />*/}
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
       </Head>
       <body>
         <Main />
--- theme.config.js
+++ theme.config.js
@@ -1,20 +1,36 @@
 const YEAR = new Date().getFullYear();
 
+const footer = <footer style={{ marginTop: "8rem" }}>
+  <small>
+    <time>{YEAR}</time>
+    © CodePsy-2001.
+    <a href="/feed.xml" style={{ float: "right" }}>RSS</a>
+  </small>
+</footer>
+
+const head = ({ title, meta }) => <>
+  {meta.title && <>
+    <meta property="og:title" content={meta.title}/>
+    <meta name="twitter:title" content={meta.title}/>
+  </>}
+  {meta.description && <>
+    <meta name="description" content={meta.description}/>
+    <meta property="og:description" content={meta.description}/>
+    <meta name="twitter:description" content={meta.description}/>
+  </>}
+  {meta.tag && <meta name="keywords" content={meta.tag}/>}
+  {meta.author && <meta name="author" content={meta.author} />}
+</>
+
 export default {
-  footer: (
-    <footer>
-      <small>
-        <time>{YEAR}</time> © CodePsy-2001.
-        <a href="/feed.xml">RSS</a>
-      </small>
-      <style jsx>{`
-        footer {
-          margin-top: 8rem;
-        }
-        a {
-          float: right;
-        }
-      `}</style>
-    </footer>
-  ),
+  darkMode: true,
+  head,
+  footer,
+  readMore: null,
+  // navs: [
+  //   {
+  //     url: 'https://github.com/shuding/nextra',
+  //     name: 'Nextra'
+  //   }
+  // ]
 };
