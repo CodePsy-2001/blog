4e0c42b163f6eb966f694da14db9e78f56c38e0e
CodePsy-2001 <codepsy2001@gmail.com>
2024-11-03 20:29:40 +0900

    [fix]: import nextra/page-map

--- app/posts/get-posts.ts
+++ app/posts/get-posts.ts
@@ -1,14 +1,13 @@
 import type { BlogMetadata } from 'nextra-theme-blog'
 import type { Page } from 'nextra'
+import { getPageMap } from 'nextra/page-map'
 
 type PostPage = Page & { frontMatter: BlogMetadata & { tags?: string[] } }
 
 export async function getPosts() {
-  const { pageMap } = await import(
-    '../../.next/static/chunks/nextra-page-map-.mjs'
-    ) as { pageMap: Page[] }
+  const { pageMap } = await getPageMap() as { pageMap: (Page & { children: PostPage[] })[] }
 
-  const postPage = pageMap.find(item => item.route === '/posts')! as Page & { children: PostPage[] }
+  const postPage = pageMap.find((item) => item.route === '/posts')! as Page & { children: PostPage[] }
 
   return postPage.children.filter(post => !post.frontMatter.draft && post.name !== 'index')
     .sort((a, b) => (new Date(b.frontMatter.date ?? 0)).getTime() - (new Date(a.frontMatter.date ?? 0)).getTime())
