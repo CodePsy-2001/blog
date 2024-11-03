3cab162d4f686e6053cb842c3f80bb2e89edd7cd
CodePsy-2001 <codepsy2001@gmail.com>
2024-11-03 21:04:19 +0900

    tagPage 정상화

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -1,13 +1,15 @@
 import { PostCard } from 'nextra-theme-blog'
 import { getPosts, getTags } from '../../posts/get-posts'
+import type { Metadata, ResolvingMetadata } from 'next'
 
-type PageProps = {
-  params: { tag: string }
+type Props = {
+  params: Promise<{ tag: string }>
 }
 
-export function generateMetadata({ params }: PageProps) {
+export async function generateMetadata({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+  const tag = (await params).tag
   return {
-    title: `“${decodeURIComponent(params.tag)}” 태그가 포함된 게시글`
+    title: `“${decodeURIComponent(tag)}” 태그가 포함된 게시글`
   }
 }
 
@@ -16,13 +18,15 @@ export async function generateStaticParams() {
   return [...new Set(allTags)].map(tag => ({ tag }))
 }
 
-export default async function TagPage({ params }: PageProps) {
+export default async function TagPage({ params }: Props) {
+  const tag = (await params).tag
+
   return (
     <>
-      <h1>{generateMetadata({ params }).title}</h1>
+      <h1>“{decodeURIComponent(tag)}” 태그가 포함된 게시글</h1>
       {(await getPosts())
         .filter(post =>
-          post.frontMatter!.tags!.includes(decodeURIComponent(params.tag))
+          post.frontMatter!.tags!.includes(tag)
         )
         .map(post => (
           <PostCard key={post.route} post={post} readMore="" />
